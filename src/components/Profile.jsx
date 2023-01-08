f271ab94f8e89dda012b214ec2934e69de4697d0
Dmitriy <dmitrijzavadovskij@gmail.com>
2023-01-08 17:39:02 +0200

    first commit root

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+	return(
+		<div className='content'>
+			<div>
+				<img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' alt='topImage'/>
+			</div>
+			<div>
+				ava + descriptiom
+			</div>
+			<div>
+				My post
+				<div>
+					New post
+				</div>
+			</div>
+			<div>
+				<div>post1</div>
+				<div>post2</div>
+				<div>post3</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
